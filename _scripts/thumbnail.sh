e8f8dd4c133f2018fe36b0d435eb6f561fadfe6a
Casey Link <unnamedrambler@gmail.com>
2011-07-02 15:09:25 -0400

    make 300x300 size thumbnail

--- _scripts/thumbnail.sh
+++ _scripts/thumbnail.sh
@@ -4,3 +4,4 @@ filename=$(basename $1)
 extension=${filename##*.}
 filename=${filename%%.*}
 convert $1 -resize "300" -gravity center -extent "300x175" $filename-300x175.$extension
+convert $1 -resize "300" -gravity center -extent "300x300" $filename-300x300.$extension
