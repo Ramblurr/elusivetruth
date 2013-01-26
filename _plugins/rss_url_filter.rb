2d7c618268ca00939aaf01895be10e22d808ecbe
Casey Link <unnamedrambler@gmail.com>
2013-01-26 14:21:48 +0100

    add relative url -> absolute url plugin for rss feed

---
+++ _plugins/rss_url_filter.rb
@@ -0,0 +1,19 @@
+require 'liquid'
+
+module RSSURLFilter
+
+    def relative_urls_to_absolute(content)
+      # set your site's url
+      url = "http://elusivetruth.net/"
+
+      # rewrite all src and href attributes that begin with /
+      content
+      .gsub(Regexp.quote("src='/"), "src='" + url)
+      .gsub(Regexp.quote("src=\"/"), "src=\"" + url)
+      .gsub(Regexp.quote("href='/"), "href='" + url)
+      .gsub(Regexp.quote("href=\"/"), "href=\"" + url)
+    end
+
+end
+
+Liquid::Template.register_filter(RSSURLFilter)
