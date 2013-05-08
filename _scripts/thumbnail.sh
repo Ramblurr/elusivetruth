b0d2037efc12fd279e30e21efcdf1f7fc22c8858
Casey Link <unnamedrambler@gmail.com>
2013-05-08 22:01:18 +0100

    move ice to map points

--- _scripts/thumbnail.sh
+++ _scripts/thumbnail.sh
@@ -6,4 +6,5 @@ extension=${filename##*.}
 filename=${filename%%.*}
 convert $i -resize "300" -gravity center -extent "300x175" $filename-300x175.$extension
 convert $i -resize "300" -gravity center -extent "300x300" $filename-300x300.$extension
+convert $i -resize "240" -gravity center -extent "240x133" $filename-240x133.$extension
 done
