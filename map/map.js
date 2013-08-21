8a25848c1c0cc751cb15db81cc000a237d2f0d09
Casey Link <unnamedrambler@gmail.com>
2013-08-21 13:42:25 +0200

    update to cartodb 2.1

--- map/map.js
+++ map/map.js
@@ -18,25 +18,34 @@ if (params.layers) {
   var options = {
     center: [params.lat || 20, params.lng || 0],
     zoom: params.zoom || 3,
-    zoomControl: false,  // dont add the zoom overlay (it is added by default)
+    zoomControl: true,  // dont add the zoom overlay (it is added by default)
     loaderControl: false, //dont show tiles loader
-    sql: 'select  *, ST_AsGeoJSON(the_geom) as geometry from {{table_name}}'
   };
 
-  var journey_url = 'http://elusivetruth.cartodb.com/api/v1/viz/7031/viz.json'
-  var points_url = 'http://elusivetruth.cartodb.com/api/v1/viz/7049/viz.json'
-  cartodb.createVis('map', journey_url, options)
+  var travel_url = 'http://elusivetruth.cartodb.com/api/v2/viz/d8b9da38-0a4d-11e3-8c39-3085a9a956e8/viz.json';
+  cartodb.createVis('map', travel_url, options)
     .done(function(vis, layers) {
 
       native_map = vis.getNativeMap()
       // there are two layers, base layer and points layer
-      var layer = layers[1];
-      layer.setInteractivity('cartodb_id,geometry,name,description,url,url_text');
+      var route_layer= layers[1].getSubLayer(0);
+      route_layer.setInteraction(true);
+      route_layer.infowindow.set('template', $('#infowindow_template').html());
+      route_layer.set({
+          sql: 'select  *, ST_AsGeoJSON(the_geom) as geometry from journey',
+          interactivity: 'cartodb_id,geometry,name,description,url,url_text'
+      });
 
-      // Set the custom infowindow template defined on the html
-      layer.infowindow.set('template', $('#infowindow_template').html());
+      var point_layer = layers[1].getSubLayer(1);
+      point_layer.setInteraction(true);
+      point_layer.infowindow.set('template', $('#infowindow_template').html());
+      point_layer.set({ interactivity: 'cartodb_id,name,description'});
+      point_layer.on('featureClick', function(e, pos, latlng, data) {
+          // no-op
+      });
 
-       layer.on('featureOver', function(e, pos, latlng, data) {
+
+       route_layer.on('featureOver', function(e, pos, latlng, data) {
            $('.leaflet-container').css('cursor','default')
            if( data.url != null ) {
                 $('.leaflet-container').css('cursor','pointer')
@@ -45,26 +54,16 @@ if (params.layers) {
                 drawHoverLine(data);
             }
         });
-        layer.on('featureOut', function(e, pos, latlng, data) {
+        route_layer.on('featureOut', function(e, pos, latlng, data) {
                 $('.leaflet-container').css('cursor','default')
                 removeLine();
             });
-        layer.on('featureClick', function(e, pos, latlng, data) {
+        route_layer.on('featureClick', function(e, pos, latlng, data) {
             if( data.url != null ) {
                 window.location = data.url;
             }
             });
-    cartodb.createLayer(native_map, points_url)
-      .on('done', function(layer) {
-          native_map.addLayer(layer)
-          layer.infowindow.set('template', $('#infowindow_template').html());
-          layer.setInteractivity('cartodb_id,name,description');
-          layer.on('featureClick', function(e, pos, latlng, data) {
-            console.log("clicked! " + data.img_small)
-          });
-      });
-
-      // add the tooltip show when hover on the point
+    // add the tooltip show when hover on the point
       vis.addOverlay({
         type: 'tooltip',
         template: '<p>{{name}}</p>'
